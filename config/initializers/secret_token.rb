c760db29284efa1f22cb33565118b91354682715
dakuo wang <dakuowang@gmail.com>
2014-03-01 05:38:42 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turtlewriter::Application.config.secret_key_base = 'f5ceba60b0f74f83b274acd4d1dbcb25b4148742d8ee67b8b53bcf28a2f611dd682d9f981d8fd256217a4be6d8ca24bc9e8452c8fca7967154097a9dc2845b82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turtlewriter::Application.config.session_store :cookie_store, key: '_turtlewriter_session'
